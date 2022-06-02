1e8a031484ce95658f287f9abc6a733a6e1dcd54
7SHAAARK2 <martinezfelipe.agustin+1@gmail.com>
2022-06-01 23:55:32 -0300

    header

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ instagram website clone;
 **harcoded** user, feed, explore, activity. Basically everything. No search, liking, etc.
 
 only working on mobile view for now
+
+# how to instagram clone sveltekit rewrite 100% real
+
+```
+    git clone https://github.com/7SHAAARK2/instagram-clone
+    cd instagram-clone
+    cd new
+    npm install
+    npm run dev
+```
+
+* now just go to http://localhost:3000
--- new/src/routes/index.svelte
+++ new/src/routes/index.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>Instagram</title>
+</svelte:head>
+
+<script>
+    import Header from '$lib/Header.svelte';
+</script>
+
+<main>
+    <Header />
+    <h1>brr</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</main>
+
+<style>
+    :global(body){
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+</style>
